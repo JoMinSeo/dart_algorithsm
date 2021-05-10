ac45c176aebdbeb3af6f9bdced3f91d3baf82434
JoMinSeo <49663640+JoMinSeo@users.noreply.github.com>
2021-05-10 17:29:36 +0900

    feat: 피보나치

---
+++ 프로그래머스/fibonacci.dart
@@ -0,0 +1,16 @@
+int Fibonacci(int n) {
+  int a = 0;
+  int b = 1;
+  for (int i = 0; i < n; i++) {
+    int c = (a + b) % 1234567;
+    a = b % 1234567;
+    b = c % 1234567;
+  }
+
+  return a;
+}
+
+void main() {
+  print(Fibonacci(3));
+  print(Fibonacci(5));
+}
